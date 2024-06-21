1f29b513c77c4e8c9ba4e5647bfc77b4a1e170e6
Minh Kha Tran <fpt_khatm@flyscoot.com>
2024-06-21 08:58:20 +0700

    Update Kubenetes Deploy settings

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a68534fb57eee4a6f882e6fddecd3db0-2015825597.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
